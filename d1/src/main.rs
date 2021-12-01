e9f155e094e7e370975e875e48f62be75dce3232
Marshal <marshal@motivedge.io>
2021-12-01 15:09:52 +0800

    Add readme and tests for Day1

--- d1/src/main.rs
+++ d1/src/main.rs
@@ -39,3 +39,68 @@ fn main() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{q1, q2};
+
+    #[test]
+    fn q1_empty_slice() {
+        let res = q1(&[]);
+        assert_eq!(res, 0);
+    }
+
+    #[test]
+    fn q1_one_elem() {
+        let res = q1(&[1]);
+        assert_eq!(res, 0);
+    }
+
+    #[test]
+    fn q1_descresing() {
+        let res = q1(&[5, 4, 3, 2, 1]);
+        assert_eq!(res, 0);
+    }
+
+    #[test]
+    fn q1_increasing() {
+        let res = q1(&[1, 2, 3, 4, 5]);
+        assert_eq!(res, 4);
+    }
+
+    #[test]
+    fn q1_hybrid() {
+        let res = q1(&[3, 2, 4, 4, 5]);
+        assert_eq!(res, 2);
+    }
+
+    #[test]
+    fn q2_empty_slice() {
+        let res = q2(&[]);
+        assert_eq!(res, 0);
+    }
+
+    #[test]
+    fn q2_3_elems() {
+        let res = q2(&[1, 2, 3]);
+        assert_eq!(res, 0);
+    }
+
+    #[test]
+    fn q2_descresing() {
+        let res = q2(&[5, 4, 3, 2, 1]);
+        assert_eq!(res, 0);
+    }
+
+    #[test]
+    fn q2_increasing() {
+        let res = q2(&[1, 2, 3, 4, 5]);
+        assert_eq!(res, 2);
+    }
+
+    #[test]
+    fn q2_hybrid() {
+        let res = q2(&[3, 2, 4, 1, 5]);
+        assert_eq!(res, 1);
+    }
+}
