02ffff91c50204c076814b8c848b1853c73465ec
Marshal <marshal@motivedge.io>
2021-12-20 21:37:27 +0800

    Day16 my own version

--- d16/src/main.rs
+++ d16/src/main.rs
@@ -1,5 +1,5 @@
 use anyhow;
-use std::io::{stdin, Read};
+use std::io::stdin;
 
 fn string_to_bits(input: String) -> Vec<u8> {
     let bchars = input
@@ -19,103 +19,133 @@ fn string_to_bits(input: String) -> Vec<u8> {
         .collect()
 }
 
-fn num(b: &[u8]) -> usize {
-    b.iter()
-        .enumerate()
-        .map(|(i, &x)| (x as usize) << (b.len() - i - 1))
-        .sum()
+fn num(bits: &[u8]) -> usize {
+    let s = bits.iter().map(|n| format!("{}", n)).collect::<String>();
+    usize::from_str_radix(&s, 2).unwrap()
 }
 
-fn q1(mut bits: &[u8]) -> (usize, &[u8]) {
-    let mut ver = num(&bits[0..3]);
-    let typ = num(&bits[3..6]);
-    bits = &bits[6..];
-    if typ == 4 {
-        (0..)
-            .take_while(|_| {
-                let cont = bits[0] == 1;
-                bits = &bits[5..];
-                cont
-            })
-            .count();
-        return (ver, bits);
+fn q1(bits: &[u8]) -> (usize, usize) {
+    // return version sum and total length
+    if bits.len() == 0 || bits.iter().all(|n| *n == 0) {
+        return (0, 0);
     }
 
-    let split = bits.split_at(1);
-    bits = split.1;
-    if split.0[0] == 0 {
-        let len = num(&bits[0..15]);
-        bits = &bits[15..];
-        let (mut payload, extra) = bits.split_at(len);
-        bits = extra;
-        while !payload.is_empty() {
-            let packet = q1(payload);
-            payload = packet.1;
-            ver += packet.0;
+    let version = num(&bits[0..3]);
+    let id = num(&bits[3..6]);
+
+    let (version, length) = if id == 4 {
+        let mut section_start_idx = 6;
+        loop {
+            let first_bit = bits[section_start_idx];
+            section_start_idx += 5;
+            if first_bit == 0 {
+                break;
+            }
         }
+        (version, section_start_idx)
     } else {
-        let split = bits.split_at(11);
-        bits = split.1;
-        ver += (0..num(split.0))
-            .map(|_| {
-                let packet = q1(bits);
-                bits = packet.1;
-                packet.0
-            })
-            .sum::<usize>();
-    }
-    (ver, bits)
+        match bits[6] {
+            0 => {
+                let mut data_start_idx = 7 + 15;
+                let len = num(&bits[7..data_start_idx]);
+                let total_len = data_start_idx + len;
+                let mut versions: Vec<usize> = Vec::new();
+                while data_start_idx < total_len {
+                    let sub_packets = &bits[data_start_idx..total_len];
+                    let (v, l) = q1(sub_packets);
+                    data_start_idx += l;
+                    versions.push(v);
+                }
+                let versions_sum: usize = versions.iter().sum();
+                (version + versions_sum, total_len)
+            }
+            1 => {
+                let data_start_idx = 7 + 11;
+                let times = num(&bits[7..data_start_idx]);
+                let mut length = 0;
+                let mut versions: Vec<usize> = Vec::new();
+                for _i in 0..times {
+                    let (v, l) = q1(&bits[(data_start_idx + length)..]);
+                    versions.push(v);
+                    length += l;
+                }
+                let versions_sum: usize = versions.iter().sum();
+                (version + versions_sum, data_start_idx + length)
+            }
+            _ => panic!("Should not be here!"),
+        }
+    };
+
+    (version, length)
 }
 
-fn q2(mut bits: &[u8]) -> (Vec<usize>, &[u8]) {
-    let typ = num(&bits[3..6]);
-    bits = &bits[6..];
-    if typ == 4 {
-        let mut num_bits = vec![];
-        (0..)
-            .take_while(|_| {
-                let cont = bits[0] == 1;
-                num_bits.extend(&bits[1..5]);
-                bits = &bits[5..];
-                cont
-            })
-            .count();
-        return (vec![num(&num_bits).into()], bits);
+fn q2(bits: &[u8]) -> (Vec<usize>, usize) {
+    // return value list and length
+    if bits.len() == 0 || bits.iter().all(|n| *n == 0) {
+        return (vec![], 0);
     }
 
-    let mut nums = vec![];
-    let split = bits.split_at(1);
-    bits = split.1;
-    if split.0[0] == 0 {
-        let len = num(&bits[0..15]) as usize;
-        bits = &bits[15..];
-        let (mut payload, extra) = bits.split_at(len);
-        bits = extra;
-        while !payload.is_empty() {
-            let packet = q2(payload);
-            payload = packet.1;
-            nums.extend_from_slice(&packet.0);
+    let id = num(&bits[3..6]);
+
+    let (value_list, length): (Vec<usize>, usize) = if id == 4 {
+        let mut section_start_idx = 6;
+        let mut values: Vec<u8> = Vec::new();
+        loop {
+            let first_bit = bits[section_start_idx];
+            values.extend(&bits[section_start_idx + 1..section_start_idx + 5]);
+            section_start_idx += 5;
+            if first_bit == 0 {
+                break;
+            }
         }
+        let value = num(&values);
+        return ([value].to_vec(), section_start_idx);
     } else {
-        let split = bits.split_at(11);
-        bits = split.1;
-        (0..num(split.0)).for_each(|_| {
-            let packet = q2(bits);
-            bits = packet.1;
-            nums.extend_from_slice(&packet.0);
-        });
-    }
+        match bits[6] {
+            0 => {
+                let mut data_start_idx = 7 + 15;
+                let len = num(&bits[7..data_start_idx]);
+                let total_len = data_start_idx + len;
+                let mut values: Vec<usize> = Vec::new();
+                while data_start_idx < total_len {
+                    let sub_packets = &bits[data_start_idx..total_len];
+                    let (v, l) = q2(sub_packets);
+                    data_start_idx += l;
+                    values.extend(v);
+                }
+                (values, total_len)
+            }
+            1 => {
+                let data_start_idx = 7 + 11;
+                let times = num(&bits[7..data_start_idx]);
+                let mut length = 0;
+                let mut values: Vec<usize> = Vec::new();
+                for _i in 0..times {
+                    let (v, l) = q2(&bits[(data_start_idx + length)..]);
+                    values.extend(v);
+                    length += l;
+                }
+                (values, data_start_idx + length)
+            }
+            _ => panic!("Should not be here!"),
+        }
+    };
 
-    match typ {
-        0 => (vec![nums.iter().sum()], bits),
-        1 => (vec![nums.iter().product()], bits),
-        2 => (vec![*nums.iter().min().unwrap()], bits),
-        3 => (vec![*nums.iter().max().unwrap()], bits),
-        5 => (vec![(nums[0] > nums[1]) as usize], bits),
-        6 => (vec![(nums[0] < nums[1]) as usize], bits),
-        7 => (vec![(nums[0] == nums[1]) as usize], bits),
-        _ => unreachable!(),
-    }
+    let value = match id {
+        0 => value_list.iter().sum(),
+        1 => value_list.iter().fold(1, |acc, x| acc * x),
+        2 => *value_list.iter().min().unwrap(),
+        3 => *value_list.iter().max().unwrap(),
+        4 => {
+            return (value_list, length);
+        }
+        5 => (value_list[0] > value_list[1]) as usize,
+        6 => (value_list[0] < value_list[1]) as usize,
+        7 => (value_list[0] == value_list[1]) as usize,
+        _ => panic!("Should not be here!"),
+    };
+
+    (vec![value], length)
 }
 
 fn main() -> anyhow::Result<()> {
@@ -124,10 +154,10 @@ fn main() -> anyhow::Result<()> {
     let bits = string_to_bits(buffer);
 
     let q1_res = q1(&bits);
-    println!("{:?}", q1_res.0);
+    println!("Q1: {:?}", q1_res.0);
 
     let q2_res = q2(&bits);
-    println!("{}", q2_res.0[0]);
+    println!("Q2: {:?}", q2_res.0[0]);
 
     Ok(())
 }
